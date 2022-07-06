1ad038cd82407b10488f983336979a1f5ef296d6
jtsarvida <jtsarvida@gmail.com>
2022-07-06 00:32:44 -0400

    commit tree 300627c28ac31e704e2ec93815a0861e14f48222

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
 # Add  code here!
+def prime?(int)
+    not_prime_condition = [0,1]
+    if int == 2
+        return true
+    elsif int.negative?
+        return false
+    elsif not_prime_condition.include?(int)
+        return false
+    else
+        divisors = (1..int).select { |i| int % i == 0 } 
+        if divisors.size > 2
+            return false
+        else
+            return true
+        end
+    end
+
+
+end
